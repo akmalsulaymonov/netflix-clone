63721ee10e33bc36fd4a89f16ce3fc55dce7407f
akmalsulaymonov <akmalsulaymonov@gmail.com>
2023-08-18 12:57:14 +0500

    Movie List & Movie Card Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage, NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
-import useCurrentUser from '../hooks/useCurrentUser';
 import Navbar from "../components/Navbar";
 import Billboard from "../components/Billboard";
+import MovieList from "../components/MovieList";
+import useMovieList from '../hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
@@ -23,12 +24,15 @@ export async function getServerSideProps(context: NextPageContext){
 
 const Home: NextPage = () => {
 
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
